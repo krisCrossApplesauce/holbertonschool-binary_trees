60672735435c3315c65f3f8190af8518248c4b28
krisCrossApplesauce <karisrr2203@gmail.com>
2023-04-23 11:27:03 -0700

    8-binary_tree_postorder.c: betty >,'/

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
+ * binary_tree_postorder - goes through a binary tree
+ * using post-order traversal
  *
  * @tree: binary_tree *, pointer to the root node of the tree to traverse
  * @func: void (*func)(int), pointer to a function to call for each node,
